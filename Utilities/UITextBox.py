6a96b395211407b7ae6a56d738be16f98596549f
Alain Micalaudie <alain@micalaudie.com>
2020-01-22 15:50:46 +0100

    Added venv and UITextBox

--- Utilities/UITextBox.py
+++ Utilities/UITextBox.py
@@ -1,8 +1,13 @@
 """
-Copyright 2017, Silas Gyger, silasgyger@gmail.com, All rights reserved.
-Borrowed from https://github.com/Nearoo/pygame-text-input under the MIT license.
+    Inspired from :
+
+    Copyright 2017, Silas Gyger, silasgyger@gmail.com, All rights reserved.
+    Borrowed from https://github.com/Nearoo/pygame-text-input under the MIT license.
+
+    Note : for PyGame 1.9.x
 """
 
+import sys
 import os.path
 
 import pygame
@@ -13,195 +18,219 @@ pygame.font.init()
 
 class TextInput:
     """
-    This class lets the user input a piece of text, e.g. a name or a message.
-    This class let's the user input a short, one-lines piece of text at a blinking cursor
-    that can be moved using the arrow-keys. Delete, home and end work as well.
+        This class let's the user input a short, one-lines piece of text at a blinking cursor
+        that can be moved using the arrow-keys. 
+        Delete, backspace, home and end are functional.
+        Return returns 
     """
     def __init__(
             self,
-            initial_string="",
-            font_family="",
-            font_size=35,
-            antialias=True,
-            text_color=(0, 0, 0),
-            cursor_color=(0, 0, 1),
-            repeat_keys_initial_ms=400,
-            repeat_keys_interval_ms=35,
-            max_string_length=-1):
+            InitialString="",
+            FontFamily="",
+            FontSize=20,
+            Antialias=True,
+            TextColor=(0, 0, 0),
+            CursorColor=(0, 0, 1),
+            RepeatKeysInitialDelay=400,
+            RepeatKeysInterval=35,
+            MaxStringLength=-1):
         """
-        :param initial_string: Initial text to be displayed
-        :param font_family: name or list of names for font (see pygame.font.match_font for precise format)
-        :param font_size:  Size of font in pixels
-        :param antialias: Determines if antialias is applied to font (uses more processing power)
-        :param text_color: Color of text (duh)
-        :param cursor_color: Color of cursor
-        :param repeat_keys_initial_ms: Time in ms before keys are repeated when held
-        :param repeat_keys_interval_ms: Interval between key press repetition when held
-        :param max_string_length: Allowed length of text
+            :param InitialString: Initial text to be displayed
+            :param FontFamily: name or list of names for font (see pygame.font.match_font for precise format)
+            :param FontSize:  Size of font in pixels
+            :param Antialias: Determines if antialias is applied to font (uses more processing power)
+            :param TextColor: Color of text (duh)
+            :param CursorColor: Color of cursor
+            :param RepeatKeysInitialDelay: Time in ms before keys are repeated when held
+            :param RepeatKeysInterval: Interval between key press repetition when held
+            :param MaxStringLength: Allowed length of text (-1 = no limit)
         """
 
-        # Text related vars:
-        self.antialias = antialias
-        self.text_color = text_color
-        self.font_size = font_size
-        self.max_string_length = max_string_length
-        self.input_string = initial_string  # Inputted text
+        # Text variables
+        self.Antialias = Antialias
+        self.TextColor = TextColor
+        self.FontSize = FontSize
+        self.MaxStringLength = MaxStringLength
+        self.InputString = InitialString  # Input text
 
-        if not os.path.isfile(font_family):
-            font_family = pygame.font.match_font(font_family)
-
-        self.font_object = pygame.font.Font(font_family, font_size)
+        # Get font
+        if not os.path.isfile(FontFamily):
+            FontFamily = pygame.font.match_font(FontFamily)
+        self.FontObject = pygame.font.Font(FontFamily, FontSize)
 
         # Text-surface will be created during the first update call:
-        self.surface = pygame.Surface((1, 1))
-        self.surface.set_alpha(0)
+        self.Surface = pygame.Surface((1, 1))
+        self.Surface.set_alpha(0)
 
-        # Vars to make keydowns repeat after user pressed a key for some time:
-        self.keyrepeat_counters = {}  # {event.key: (counter_int, event.unicode)} (look for "***")
-        self.keyrepeat_intial_interval_ms = repeat_keys_initial_ms
-        self.keyrepeat_interval_ms = repeat_keys_interval_ms
+        # Variables to manage key repeat after specified delay
+        self.KeyRepeatCounters = {}  # {event.key: (counter_int, event.unicode)} (look for "***")
+        self.KeyRepeatIntialDelay = RepeatKeysInitialDelay
+        self.KeyRepeatInterval = RepeatKeysInterval
 
-        # Things cursor:
-        self.cursor_surface = pygame.Surface((int(self.font_size / 20 + 1), self.font_size))
-        self.cursor_surface.fill(cursor_color)
-        self.cursor_position = len(initial_string)  # Inside text
-        self.cursor_visible = True  # Switches every self.cursor_switch_ms ms
-        self.cursor_switch_ms = 500  # /|\
-        self.cursor_ms_counter = 0
+        # Cursor variables
+        self.CursorSurface = pygame.Surface((int(self.FontSize / 20 + 1), self.FontSize))
+        self.CursorSurface.fill(CursorColor)
+        self.CursorPosition = len(InitialString)
+        self.CursorVisible = True  # switches every CursorBlinkDelay ms
+        self.CursorBlinkDelay = 500
+        self.CursorBlinkTime = 0
 
-        self.clock = pygame.time.Clock()
+        self.Clock = pygame.time.Clock()
 
-    def update(self, events):
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                self.cursor_visible = True  # So the user sees where he writes
+    def Update(self, EventList):
 
-                # If none exist, create counter for that key:
-                if event.key not in self.keyrepeat_counters:
-                    self.keyrepeat_counters[event.key] = [0, event.unicode]
+        for CurrentEvent in EventList:
+            if CurrentEvent.type == pygame.KEYDOWN:
+                # show cursor
+                self.CursorVisible = True  
 
-                if event.key == pl.K_BACKSPACE:
-                    self.input_string = (
-                        self.input_string[:max(self.cursor_position - 1, 0)]
-                        + self.input_string[self.cursor_position:]
-                    )
+                # if none exist, create a counter for that key
+                if CurrentEvent.key not in self.KeyRepeatCounters:
+                    self.KeyRepeatCounters[CurrentEvent.key] = [0, CurrentEvent.unicode]
 
-                    # Subtract one from cursor_pos, but do not go below zero:
-                    self.cursor_position = max(self.cursor_position - 1, 0)
-                elif event.key == pl.K_DELETE:
-                    self.input_string = (
-                        self.input_string[:self.cursor_position]
-                        + self.input_string[self.cursor_position + 1:]
-                    )
-
-                elif event.key == pl.K_RETURN:
+                if CurrentEvent.key == pl.K_RETURN or CurrentEvent.key == pl.K_KP_ENTER:
+                    # return input string
                     return True
 
-                elif event.key == pl.K_RIGHT:
-                    # Add one to cursor_pos, but do not exceed len(input_string)
-                    self.cursor_position = min(self.cursor_position + 1, len(self.input_string))
-
-                elif event.key == pl.K_LEFT:
-                    # Subtract one from cursor_pos, but do not go below zero:
-                    self.cursor_position = max(self.cursor_position - 1, 0)
-
-                elif event.key == pl.K_END:
-                    self.cursor_position = len(self.input_string)
-
-                elif event.key == pl.K_HOME:
-                    self.cursor_position = 0
-
-                elif len(self.input_string) < self.max_string_length or self.max_string_length == -1:
-                    # If no special key is pressed, add unicode of key to input_string
-                    self.input_string = (
-                        self.input_string[:self.cursor_position]
-                        + event.unicode
-                        + self.input_string[self.cursor_position:]
+                elif CurrentEvent.key == pl.K_BACKSPACE:
+                    # delete previous character and move cursor back 1 space
+                    self.InputString = (
+                        self.InputString[:max(self.CursorPosition - 1, 0)]
+                        + self.InputString[self.CursorPosition:]
                     )
-                    self.cursor_position += len(event.unicode)  # Some are empty, e.g. K_UP
+                    self.CursorPosition = max(self.CursorPosition - 1, 0)
 
-            elif event.type == pl.KEYUP:
-                # *** Because KEYUP doesn't include event.unicode, this dict is stored in such a weird way
-                if event.key in self.keyrepeat_counters:
-                    del self.keyrepeat_counters[event.key]
+                elif CurrentEvent.key == pl.K_DELETE:
+                    # delete next character
+                    self.InputString = (
+                        self.InputString[:self.CursorPosition]
+                        + self.InputString[self.CursorPosition + 1:]
+                    )
+
+                elif CurrentEvent.key == pl.K_RIGHT:
+                    # move cursor 1 space forward, do not exceed string length
+                    self.CursorPosition = min(self.CursorPosition + 1, len(self.InputString))
+
+                elif CurrentEvent.key == pl.K_LEFT:
+                    # move cursor 1 space backward
+                    self.CursorPosition = max(self.CursorPosition - 1, 0)
+
+                elif CurrentEvent.key == pl.K_END:
+                    # move cursor to end of string
+                    self.CursorPosition = len(self.InputString)
+
+                elif CurrentEvent.key == pl.K_HOME:
+                    # move cursor to start of string
+                    self.CursorPosition = 0
+             
+                elif len(self.InputString) < self.MaxStringLength or self.MaxStringLength == -1:
+                    # if string does not exceed max length, add key to string
+                    if CurrentEvent.unicode is not None and len(str(CurrentEvent.unicode)) == 1:
+                        self.InputString = (
+                            self.InputString[:self.CursorPosition]
+                            + CurrentEvent.unicode
+                            + self.InputString[self.CursorPosition:]
+                        )
+                        self.CursorPosition += len(CurrentEvent.unicode)  # some unicodes may be empty, like K_UP
+                        print(CurrentEvent.unicode)
+
+            elif CurrentEvent.type == pl.KEYUP:
+                # note: because KEYUP doesn't include event.unicode, this dict is stored in such a weird way
+                if CurrentEvent.key in self.KeyRepeatCounters:
+                    del self.KeyRepeatCounters[CurrentEvent.key]
 
         # Update key counters:
-        for key in self.keyrepeat_counters:
-            self.keyrepeat_counters[key][0] += self.clock.get_time()  # Update clock
+        for Key in self.KeyRepeatCounters:
+            self.KeyRepeatCounters[Key][0] += self.Clock.get_time()  # Update clock
 
-            # Generate new key events if enough time has passed:
-            if self.keyrepeat_counters[key][0] >= self.keyrepeat_intial_interval_ms:
-                self.keyrepeat_counters[key][0] = (
-                    self.keyrepeat_intial_interval_ms
-                    - self.keyrepeat_interval_ms
+            # generate new key events if enough time has passed:
+            if self.KeyRepeatCounters[Key][0] >= self.KeyRepeatIntialDelay:
+                self.KeyRepeatCounters[Key][0] = (
+                    self.KeyRepeatIntialDelay
+                    - self.KeyRepeatInterval
                 )
 
-                event_key, event_unicode = key, self.keyrepeat_counters[key][1]
-                pygame.event.post(pygame.event.Event(pl.KEYDOWN, key=event_key, unicode=event_unicode))
+                EventKey, EventUnicode = Key, self.KeyRepeatCounters[Key][1]
+                pygame.event.post(pygame.event.Event(pl.KEYDOWN, key=EventKey, unicode=EventUnicode))
 
         # Re-render text surface:
-        self.surface = self.font_object.render(self.input_string, self.antialias, self.text_color)
+        self.Surface = self.FontObject.render(self.InputString, self.Antialias, self.TextColor)
 
-        # Update self.cursor_visible
-        self.cursor_ms_counter += self.clock.get_time()
-        if self.cursor_ms_counter >= self.cursor_switch_ms:
-            self.cursor_ms_counter %= self.cursor_switch_ms
-            self.cursor_visible = not self.cursor_visible
+        # Update cursor
+        self.CursorBlinkTime += self.Clock.get_time()
+        if self.CursorBlinkTime >= self.CursorBlinkDelay:
+            self.CursorBlinkTime %= self.CursorBlinkDelay
+            self.CursorVisible = not self.CursorVisible
 
-        if self.cursor_visible:
-            cursor_y_pos = self.font_object.size(self.input_string[:self.cursor_position])[0]
-            # Without this, the cursor is invisible when self.cursor_position > 0:
-            if self.cursor_position > 0:
-                cursor_y_pos -= self.cursor_surface.get_width()
-            self.surface.blit(self.cursor_surface, (cursor_y_pos, 0))
+        if self.CursorVisible:
+            CursorY = self.FontObject.size(self.InputString[:self.CursorPosition])[0]
+            # note: without this, the cursor is invisible when cursor position > 0
+            if self.CursorPosition > 0:
+                CursorY -= self.CursorSurface.get_width()
+            self.Surface.blit(self.CursorSurface, (CursorY, 0))
 
-        self.clock.tick()
+        # Add delay and return to main loop
+        self.Clock.tick()
         return False
 
-    def get_surface(self):
-        return self.surface
 
-    def get_text(self):
-        return self.input_string
-
-    def get_cursor_position(self):
-        return self.cursor_position
-
-    def set_text_color(self, color):
-        self.text_color = color
-
-    def set_cursor_color(self, color):
-        self.cursor_surface.fill(color)
-
-    def clear_text(self):
-        self.input_string = ""
-        self.cursor_position = 0
+    def GetSurface(self):
+        return self.Surface
 
 
+    def GetText(self):
+        return self.InputString
 
+
+    def GetCursorPosition(self):
+        return self.CursorPosition
+
+
+    def SetTextColor(self, Color):
+        self.TextColor = Color
+
+
+    def SetCursorColor(self, Color):
+        self.CursorSurface.fill(Color)
+
+
+    def ClearText(self):
+        self.InputString = ""
+        self.CursorPosition = 0
+
+
+# Self testing
 if __name__ == "__main__":
     pygame.init()
 
-    # Create TextInput-object
-    textinput = TextInput()
+    # create TextInput object
+    TextInputTest = TextInput(MaxStringLength=30)
 
-    screen = pygame.display.set_mode((1000, 200))
-    clock = pygame.time.Clock()
+    # initialize screen and event clock with PyGame
+    Screen = pygame.display.set_mode((1000, 200))
+    Clock = pygame.time.Clock()
 
+    # main loop
     while True:
-        screen.fill((225, 225, 225))
+        Screen.fill((225, 225, 225))
 
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                exit()
+        # get PyGame events
+        Events = pygame.event.get()
+        for Event in Events:
+            # exit when user clicks on windows red cross
+            if Event.type == pygame.QUIT:
+                sys.exit(0)
 
-        # Feed it with events every frame
-        if textinput.update(events):
-            print(textinput.get_text())
+        # feed TextInput with events and return input text if Return is pressed
+        if TextInputTest.Update(Events):
+            # print input text in console
+            print(TextInputTest.GetText())
             
-        # Blit its surface onto the screen
-        screen.blit(textinput.get_surface(), (10, 10))
+        # blit its surface onto the screen
+        Screen.blit(TextInputTest.GetSurface(), (10, 10))
 
+        # update screen
         pygame.display.update()
-        clock.tick(30)
+
+        # event delay
+        Clock.tick(30)
